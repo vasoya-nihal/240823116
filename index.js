8ca171f6003dcde8d5e4043e7d75a93803d77d47
nihal <nihalvasoya752004@gmail.com>
2025-07-14 11:21:00 +0530

    1 porgram

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const surname="vasoya"
 const  name = "nihal"
 console.log(name)
 console.log(surname)
+
+
+
+// 1
+let num = 7
+
+if(num % 2 == 0){
+    console.log("number is even")
+}else{
+    console.log("Number is odd")
+}
